91c1af6668a33437bdddc864dba72f26ceef46a9
wrightwells <wrightwells@hotmail.com>
2024-08-19 20:45:03 +0100

    added addToBuffer and flushBuffer

--- Software/src/devboard/utils/LogToSD.h
+++ Software/src/devboard/utils/LogToSD.h
@@ -6,7 +6,7 @@
 #include <FS.h>
 #include "../../lib/SD/src/SD.h"
 
-void setupLogToSD(void);
+void setupLogToSD(const String &BatteryName);
 
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels);
 void createDir(fs::FS &fs, const char *path);
@@ -16,5 +16,8 @@ void writeFile(fs::FS &fs, const char *path, const char *message);
 void appendFile(fs::FS &fs, const char *path, const char *message);
 void renameFile(fs::FS &fs, const char *path1, const char *path2);
 void deleteFile(fs::FS &fs, const char *path);
+
+void addToBuffer(const String &logData);
+void flushBufferToSD();
 
 #endif // LOG_TO_SD_H
